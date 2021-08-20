99a24fabd448930dce157ee25c4952017e5e1cbe
happiguru <hguruman@gmail.com>
2021-08-20 09:50:49 +0400

    Update website colors

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a42833a0f47cdee9d4ab";
+/******/ 	var hotCurrentHash = "9beaed4bf316368b2dd9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
