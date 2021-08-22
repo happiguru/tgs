42ec3a0c8e3a815e21dcd4e2b2823db563aad28a
happiguru <hguruman@gmail.com>
2021-08-22 12:20:35 +0400

    Update site color to dark.

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d2849bb904fa6f84ea64";
+/******/ 	var hotCurrentHash = "846a2a43c5ba020e8cb3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
