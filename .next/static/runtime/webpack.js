543eec3fdbc3bf04724d4c25b0710aa514eefc12
happiguru <hguruman@gmail.com>
2021-12-15 13:25:57 +0400

    Change contact numbers

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "19c997f9f5d7927c50dd";
+/******/ 	var hotCurrentHash = "f10a36f4e59c0ddd1636";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
